483bd41866bd696184e3026c67ace5098683253b
wastecat <6185988+zwh8800@users.noreply.github.com>
2021-06-21 18:56:53 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/zwh8800/tap-over-https
+
+go 1.16
+
+require (
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/songgao/water"
+	"nhooyr.io/websocket"
+)
+
+var (
+	cmdIsServer bool
+	cmdAddr     string
+)
+
+func main() {
+	parseCmd()
+
+	iface, err := water.New(water.Config{
+		DeviceType: water.TUN,
+	})
+	if err != nil {
+		log.Panicf("error on water new: %s", err.Error())
+	}
+
+	log.Printf("iface: %s", iface.Name())
+
+	if !cmdIsServer {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+
+		ws, _, err := websocket.Dial(ctx, cmdAddr, nil)
+		if err != nil {
+			log.Panicf("error on websocket.Dial: %s", err.Error())
+		}
+		defer ws.Close(websocket.StatusInternalError, "the sky is falling")
+
+		connectTunnel(ws, iface)
+	} else {
+		http.HandleFunc("/vpn", func(w http.ResponseWriter, r *http.Request) {
+			ws, err := websocket.Accept(w, r, nil)
+			if err != nil {
+				log.Panicf("error on websocket.Accept: %s", err.Error())
+			}
+			defer ws.Close(websocket.StatusInternalError, "the sky is falling")
+			connectTunnel(ws, iface)
+		})
+		http.ListenAndServe(cmdAddr, nil)
+	}
+
+	//packet := make([]byte, 2000)
+	//for {
+	//	n, err := iface.Read(packet)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	log.Printf("Packet Received: % x\n", packet[:n])
+	//}
+}
+
+func connectTunnel(ws *websocket.Conn, iface *water.Interface) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	_, wsReader, err := ws.Reader(ctx)
+	if err != nil {
+		log.Panicf("error on ws.Reader: %s", err.Error())
+	}
+
+	wsWriter, err := ws.Writer(ctx, websocket.MessageBinary)
+	if err != nil {
+		log.Panicf("error on ws.Writer: %s", err.Error())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, err := io.Copy(wsWriter, iface)
+		if err != nil {
+			log.Panicf("error on io.Copy(wsWriter, iface): %s", err.Error())
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, err := io.Copy(iface, wsReader)
+		if err != nil {
+			log.Panicf("error on io.Copy(iface, wsReader): %s", err.Error())
+		}
+	}()
+	wg.Wait()
+}
+
+func parseCmd() {
+	flag.BoolVar(&cmdIsServer, "s", false, "run as server mode")
+	flag.StringVar(&cmdAddr, "addr", ":8012", "vpn server address in client mode\nbind address in server mode")
+	flag.Parse()
+}
