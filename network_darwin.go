71bba756cb26c0cc5b91b082fa475ef1544cf6f7
wastecat <6185988+zwh8800@users.noreply.github.com>
2021-06-22 11:14:31 +0800

    子网掩码

--- network_darwin.go
+++ network_darwin.go
@@ -16,7 +16,7 @@ func setupTapAddr(tapName string, ipBody *IPAssignBody) {
 		log.Panicf("assigned ip not valid: %s", ipBody.IP)
 	}
 
-	cmd := exec.Command("ifconfig", tapName, ip.To4().String(), "up")
+	cmd := exec.Command("ifconfig", tapName, ip.To4().String()+"/24", "up")
 	err := cmd.Run()
 	if err != nil {
 		log.Panicf("cmd.Run error: %s", err.Error())
