e5af05483cab0487de29806a8ab3225fdd5f6620
wastecat <6185988+zwh8800@users.noreply.github.com>
2021-06-28 15:25:49 +0800

    后台运行

--- cmd/tap-over-https-gui/Taps.app/Contents/MacOS/run_as_root.sh
+++ cmd/tap-over-https-gui/Taps.app/Contents/MacOS/run_as_root.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 taps=$(dirname $0)/Taps
 /usr/bin/osascript <<EOF
-do shell script "$taps" with prompt "启动Taps需要授权" with administrator privileges
+do shell script "$taps > /dev/null 2>&1 &" with prompt "启动Taps需要授权" with administrator privileges
 EOF
